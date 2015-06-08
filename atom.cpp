1b7909c3d66538ceecaa0b802a09e6e68ccf39bd
LONELY-WOLF <zvenayte@pisem.net>
2015-06-08 17:53:12 +0300

    extract functions fixes and optimization

--- atom.cpp
+++ atom.cpp
@@ -12,7 +12,9 @@
 void read();
 int parsePacket();
 uint8_t extract_u8(uint16_t buf_off, uint32_t bit_off, uint8_t bit_len);
-uint16_t extract_u16(uint16_t buf_off, uint8_t bit_off, uint8_t bit_len);
+uint16_t extract_u16(uint16_t buf_off, uint32_t bit_off, uint8_t bit_len);
+uint32_t extract_u32(uint16_t buf_off, uint32_t bit_off, uint8_t bit_len);
+uint64_t extract_u56(uint16_t buf_off, uint32_t bit_off, uint8_t bit_len);
 
 uint8_t buffer[2048];
 uint16_t buffer_p = 0;
@@ -57,7 +59,6 @@ int parsePacket()
 	//printf("ID = %d\n", extract_u16(&buffer[buffer_p], 24, 12));
 	if (id == 4095) //Ashtech message
 	{
-		printf("Ashtech\n");
 		mes_hdr.length = extract_u16(buffer_p, 14, 10);
 		printf("length: %d\n", mes_hdr.length);
 		CHECK_DATA(mes_hdr.length + 6);
@@ -125,61 +126,60 @@ int parsePacket()
 inline uint8_t extract_u8(uint16_t buf_off, uint32_t bit_off, uint8_t bit_len)
 {
 	uint32_t offset = bit_off % 8;
-	uint16_t retval = buffer[(buf_off + (bit_off >> 3)) & 0x3FF] << 8;
+	uint16_t retval = buffer[(buf_off + (bit_off >> 3)) & 0x3FF];
+	retval <<= 8;
 	retval += buffer[(buf_off + (bit_off >> 3) + 1) & 0x3FF];
 	retval >>= (16 - bit_len - offset);
 	retval &= bitmask[bit_len - 1];
 	return (uint8_t)retval;
 }
 
-inline uint16_t extract_u16(uint16_t buf_off, uint8_t bit_off, uint8_t bit_len)
+inline uint16_t extract_u16(uint16_t buf_off, uint32_t bit_off, uint8_t bit_len)
 {
 	uint32_t offset = bit_off % 8;
-	uint32_t retval = buffer[(buf_off + (bit_off >> 3)) & 0x3FF] << 24;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 1)) & 0x3FF] << 16;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 2)) & 0x3FF] << 8;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 3)) & 0x3FF];
-	retval = retval >> (32 - bit_len - offset);
+	uint32_t retval = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		retval <<= 8;
+		retval += buffer[(buf_off + (bit_off >> 3) + i) & 0x3FF];
+	}
+	retval = retval >> (24 - bit_len - offset);
 	uint16_t mask = (uint16_t)bitmask[bit_len - 9];
 	mask <<= 8;
 	mask |= 0xFF;
-	return (uint16_t)(retval & mask);
+	return ((uint16_t)retval) & mask;
 }
 
-inline uint32_t extract_u32(uint16_t buf_off, uint8_t bit_off, uint8_t bit_len)
+inline uint32_t extract_u32(uint16_t buf_off, uint32_t bit_off, uint8_t bit_len)
 {
 	uint32_t offset = bit_off % 8;
-	//May be there is no need to read 8 bytes?
-	uint64_t retval = buffer[(buf_off + (bit_off >> 3)) & 0x3FF] << 56;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 1)) & 0x3FF] << 48;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 2)) & 0x3FF] << 40;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 1)) & 0x3FF] << 32;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 2)) & 0x3FF] << 24;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 1)) & 0x3FF] << 16;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 2)) & 0x3FF] << 8;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 3)) & 0x3FF];
-	retval = retval >> (64 - bit_len - offset);
+	uint64_t retval = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		retval <<= 8;
+		retval += buffer[(buf_off + (bit_off >> 3) + i) & 0x3FF];
+	}
+	retval = retval >> (40 - bit_len - offset);
 	uint32_t mask = 1;
 	for (uint8_t i = 1; i < bit_len; i++)
 	{
 		mask <<= 1;
 		mask++;
 	}
-	return (uint32_t)(retval & mask);
+	return ((uint32_t)retval) & mask;
 }
 
 //MADNESS!!!
-inline uint64_t extract_u64(uint16_t buf_off, uint8_t bit_off, uint8_t bit_len)
+
+inline uint64_t extract_u56(uint16_t buf_off, uint32_t bit_off, uint8_t bit_len)
 {
 	uint32_t offset = bit_off % 8;
-	uint64_t retval = buffer[(buf_off + (bit_off >> 3)) & 0x3FF] << 56;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 1)) & 0x3FF] << 48;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 2)) & 0x3FF] << 40;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 1)) & 0x3FF] << 32;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 2)) & 0x3FF] << 24;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 1)) & 0x3FF] << 16;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 2)) & 0x3FF] << 8;
-	retval += buffer[(buf_off + ((bit_off >> 3) + 3)) & 0x3FF];
+	uint64_t retval = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		retval <<= 8;
+		retval += buffer[(buf_off + (bit_off >> 3) + i) & 0x3FF];
+	}
 	retval = retval >> (64 - bit_len - offset);
 	uint64_t mask = 1;
 	for (uint8_t i = 1; i < bit_len; i++)
